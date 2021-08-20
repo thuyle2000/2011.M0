b7078d30d604cf4bede2205b1c64b6628d898f19
Le Mong Thuy <thuyle2000@gmail.com>
2021-08-20 09:02:58 +0700

    Upload Aug 20 2021

---
+++ d04/d04_bitwise.php
@@ -0,0 +1,14 @@
+<?php 
+$a =19; $b=22;
+
+echo "a= $a <br>";
+echo "b= $b <br><br>";
+
+echo "a AND b = ". ($a & $b) . "<br>";
+echo "a OR b = ". ($a | $b) . "<br>";
+echo "a XOR b = ". ($a ^ $b) . "<br>";
+echo "a >> 2 = ". ($a >> 2) . "<br>";
+echo "b << 2 = ". ($b << 2) . "<br>";
+
+
+?>  
