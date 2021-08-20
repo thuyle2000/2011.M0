26bde8ac69b017a3950ea5b73d5176b69ef0e85c
Le Mong Thuy <thuyle2000@gmail.com>
2021-08-20 09:48:20 +0700

    Upload Aug, 20 2021

---
+++ d04/d04_gptbac1.php
@@ -0,0 +1,66 @@
+<?php 
+//kiem tra trang duoc submit chua
+if(isset($_GET["btOK"])){
+    $a = $_GET["hsa"];
+    $b = $_GET["hsb"];
+
+    $m = "<p> >> PT bac 1: ". $a. "x + " . $b. "= 0 <p>";
+    echo $m;
+
+    if($b == 0){
+        if($a==0){
+            echo " >> PT vo so nghiem ! <br>";
+        }
+        else{
+            echo " >> PT vo nghiem ! <br>";
+        }
+    }
+    else{
+        echo " >> Nghiem cua PT la ". (-$b/$a) ." ! <br>";
+    }
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GPTB1</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h2>Giai phuong trinh bac 1: ax + b = 0</h2>
+        <hr>
+
+        <div class="row">
+            <div class="col-md-6">
+                <form action="">
+                    <div class="form-group">
+                        <label for="">he so a</label>
+                        <input type="number" class="form-control" name="hsa" id="hsa" value="2" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">he so b</label>
+                        <input type="number" class="form-control" name="hsb" id="hsb" value="3" required>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary" name="btOK">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ d04/d04_increament.php
@@ -0,0 +1,17 @@
+<?php 
+ echo "<h2>30 so nguyen dau tien </h2>";
+
+ for ($i=1; $i <=30 ; $i++) { 
+     echo $i . ", ";
+ }
+echo "<br><br>";
+
+echo "<h2>30 ky tu dau tien </h2>";
+
+for ($a='A', $i=1; $i <=30 ; $i++) { 
+    echo $a . ", ";
+    $a++;
+}
+echo "<br><br>";
+
+?>
